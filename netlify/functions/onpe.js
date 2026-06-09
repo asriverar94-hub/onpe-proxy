32df7dd4d8917bc4818f5c039899077dae1de66f
asriverar94-hub <asriverar94@gmail.com>
2026-06-09 15:16:52 -0500

    Enhance fetch request headers in onpe.js
    Updated the fetch request headers to include more detailed user agent and additional headers.

--- netlify/functions/onpe.js
+++ netlify/functions/onpe.js
@@ -4,7 +4,10 @@ exports.handler = async function(event, context) {
       "https://resultadosegundavuelta.onpe.gob.pe/presentacion-backend/resumen-general/totales?idEleccion=10&tipoFiltro=eleccion",
       {
         headers: {
-          "User-Agent": "Mozilla/5.0"
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/145.0.0.0 Safari/537.36",
+          "Accept": "*/*",
+          "Accept-Language": "es,es-ES;q=0.9,en;q=0.8",
+          "Referer": "https://resultadosegundavuelta.onpe.gob.pe/main/resumen"
         }
       }
     );
@@ -14,14 +17,18 @@ exports.handler = async function(event, context) {
     return {
       statusCode: 200,
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "text/plain",
         "Access-Control-Allow-Origin": "*"
       },
       body: text
     };
+
   } catch (error) {
     return {
       statusCode: 500,
+      headers: {
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify({
         error: error.message
       })
