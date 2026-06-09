04eeef7acb95b2adc6283a5f6db712dbca18bf5a
asriverar94-hub <asriverar94@gmail.com>
2026-06-09 14:08:15 -0500

    Update onpe.js

--- netlify/functions/onpe.js
+++ netlify/functions/onpe.js
@@ -1,9 +1,30 @@
 exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      ok: true,
-      mensaje: "Funcion Netlify funcionando"
-    })
-  };
+  try {
+    const response = await fetch(
+      "https://resultadosegundavuelta.onpe.gob.pe/presentacion-backend/resumen-general/totales?idEleccion=10&tipoFiltro=eleccion",
+      {
+        headers: {
+          "User-Agent": "Mozilla/5.0"
+        }
+      }
+    );
+
+    const text = await response.text();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: text
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message
+      })
+    };
+  }
 };
