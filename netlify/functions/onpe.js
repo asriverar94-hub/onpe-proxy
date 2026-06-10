9c229d2eff4b39419e54fb563f74a6654755b167
asriverar94-hub <asriverar94@gmail.com>
2026-06-09 21:00:13 -0500

    Refactor handler function in onpe.js

--- netlify/functions/onpe.js
+++ netlify/functions/onpe.js
@@ -1,14 +1,15 @@
-exports.handler = async function(event, context) {
+exports.handler = async function() {
   try {
+
     const response = await fetch(
       "https://resultadosegundavuelta.onpe.gob.pe/presentacion-backend/resumen-general/totales?idEleccion=10&tipoFiltro=eleccion",
       {
         headers: {
-          "accept": "*/*",
-          "accept-language": "es,es-ES;q=0.9,en;q=0.8",
-          "content-type": "application/json",
-          "referer": "https://resultadosegundavuelta.onpe.gob.pe/main/resumen",
-          "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/145.0.0.0 Safari/537.36"
+          "Accept": "*/*",
+          "Accept-Language": "es,es-ES;q=0.9",
+          "Content-Type": "application/json",
+          "Referer": "https://resultadosegundavuelta.onpe.gob.pe/main/resumen",
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/145.0.0.0 Safari/537.36"
         }
       }
     );
@@ -29,11 +30,13 @@ exports.handler = async function(event, context) {
     };
 
   } catch (error) {
+
     return {
       statusCode: 500,
       body: JSON.stringify({
         error: error.message
       })
     };
+
   }
 };
